3656e16f075648f71a022a0dff0726719b571672
Jose Neil Silagan <neilsilagan@gmail.com>
2024-05-20 12:12:10 +0800

    Create function operate
    takes an operator and 2 numbers and then calls one of the above functions(basic math operators) on the numbers.

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ function divide(a, b){
 let firstNum = '';
 let operator = '';
 let secondNum = '';
+
+function operate(operator, firstNum, secondNum){
+    switch(operator){
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum,secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+    }
+}
+
+console.log(operate('-', 12, 2));
