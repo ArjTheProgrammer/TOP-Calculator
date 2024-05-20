0af9bbda7e9830f0869ca2c11ab91936f94e6d31
Jose Neil Silagan <neilsilagan@gmail.com>
2024-05-20 11:54:28 +0800

    Add the basic math operators

---
+++ script.js
@@ -0,0 +1,18 @@
+//Basic math operators
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+console.log(divide(12,2));
